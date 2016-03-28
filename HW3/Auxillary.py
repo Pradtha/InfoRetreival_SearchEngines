13bfb1b2683aaa9416b2434426dc66fdbd5c7174
Pradtha <pradeep1005@hotmail.com>
2016-03-27 20:40:21 -0700

    converted pgrnk to required format

--- HW3/Auxillary.py
+++ HW3/Auxillary.py
@@ -1,10 +1,28 @@
 import sys
 import csv
 from collections import defaultdict
+import networkx as nx
+
 
 csvFile = open('pagerankdata.csv',"r")
-csvFile.seek(0);
+csvFile.seek(0)
 
-temp = []
+g = nx.DiGraph()
 reader = csv.DictReader(csvFile)	
 for row in reader:
+	g.add_edge(str(row['Page']),str(row['Outgoing Links']))
+csvFile.close()
+
+
+csvFile = open('PageRankData.csv',"w")
+csvFile.seek(0)
+csvFile.write("Page,Outgoing Links\n")
+
+for n in g.nodes():
+	s = str(n)
+	for e in g.neighbors(n):
+		s = s + "," + str(e) 
+	s = s + "\n";
+	csvFile.write(s)
+csvFile.close()
+
